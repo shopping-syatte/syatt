4f5ba06e6f39622b4e5d4dc8b250bccd234c7f6f
ruddnjs3769 <84277185+ruddnjs3769@users.noreply.github.com>
2023-08-24 21:40:00 +0900

    feat: 충돌에러 해결

--- src/components/ClassList.jsx
+++ src/components/ClassList.jsx
@@ -1,17 +1,24 @@
 import dayjs from 'dayjs';
+import { useNavigate } from 'react-router-dom';
 
 // eslint-disable-next-line react/prop-types
 export default function ClassList({ item }) {
-  function handleClick () {
-    console.log("영상보기");
+  const navigate = useNavigate();
+  // 날짜 체크 부분
+  // 수강종료일 조건을 체크하고 else 시청스타트 체크해서 시작되었으면  시청종료일 조건부 체크해서
+  // 시청 또는 구매내역으로 데이터 입력하고 기존 페이먼트에서 삭제한다.
+  function handleClick(data) {
+    navigate(`/classview/${data.vimeoId}`);
   }
-  const data = item // es-Lint error 때문에 data로 넘겨 줌
+  const data = item; // es-Lint error 때문에 data로 넘겨 줌
   return (
     <>
       <div className={'flex gap-6'}>
         <div>
           <img src={data.image} alt={data.title} />
-          <p className={'font-bold text-xl text-gray-700 text-center'}>{data.title}</p>
+          <p className={'font-bold text-xl text-gray-700 text-center'}>
+            {data.title}
+          </p>
         </div>
         <div>
           <p>{data.vimeoId}</p>
@@ -22,8 +29,10 @@ export default function ClassList({ item }) {
           {/*<p>비디오 수강 시작일 : {dayjs(data.videoStartDate).format('YYYY-MM-DD')}</p>*/}
           {/*<p>비디오 수강 종료일 : {dayjs(data.videoEndDate).format('YYYY-MM-DD')}</p>*/}
           <button
-            onClick={handleClick}
-            className={'btn btn-primary'}>강의보기
+            onClick={() => handleClick(data)}
+            className={'btn btn-primary'}
+          >
+            강의보기
           </button>
         </div>
       </div>
