935aa4806e5eaaaf2c1ac9042f43c5d2a80a6a7d
96uoow <mhs0704123@naver.com>
2023-08-24 13:27:24 +0900

    fix:fixError

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -103,6 +103,7 @@ export async function getPayments(userId) {
     });
 }
 
+
 export async function addOrUpdateToPayment(userId, product) {
   console.log(product.id);
   return set(ref(database, `payments/${userId}/${product.id}`), product);
