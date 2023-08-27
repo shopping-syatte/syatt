1eafc70fefcc7bbc72ae6e4ad9d8ccbf5606e378
pelicanStd <pelican8118@gmail.com>
2023-08-27 19:03:57 +0900

    fix: 날짜 체크 기능 에러수정

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,8 +1,20 @@
 import { useNavigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+ProductCard.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    vimeoId: PropTypes.string,
+    image: PropTypes.string.isRequired,
+    section:PropTypes.string
+  }).isRequired,
+}
 
 export default function ProductCard({ product }) {
-  const { id, image, title, category, price, vimeoId } = product; // 구조 분해 할당
-  // const vimeoId = "798314628"
+  const { id, image, title, category, price, vimeoId, section } = product;
   const navigate = useNavigate();
 
   const handleClick = () => {
@@ -12,26 +24,11 @@ export default function ProductCard({ product }) {
   return (
     <li onClick={handleClick}
       className={'rounded-lg shadow-md overflow-hidden cursor-pointer transition-all hover:scale-105'}>
-
-     {/* <div style={{ padding: "56.25% 0 0 0", position: "relative" }}>
-        <iframe
-          src={`https://player.vimeo.com/video/${vimeoId}?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479`}
-          frameBorder="0"
-          allow="autoplay; fullscreen; picture-in-picture"
-          allowFullScreen
-          style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%" }}
-          title="1-Description of the Tools.mp4"
-        ></iframe>
-        <script src="https://player.vimeo.com/api/player.js"></script>
-      </div>*/}
-
-
       <img className={'w-full'} src={image} alt={title} />
       <div className={'mt-2 px-2 text-lg flex justify-between items-center '}>
         <h3 className={'truncate'}>{title}</h3>
         <p>{`W${price}`}</p>
       </div>
-      {/*<p className={'mb-2 px-2 text-gray-600'}>{category}</p>*/}
     </li>
   )
 }
--- src/hooks/useOrderList.jsx
+++ src/hooks/useOrderList.jsx
@@ -13,7 +13,7 @@ export default function useOrderList() {
     useQuery(['orderList', uid || ''], () => getOrderList(uid),
       {
         enabled: !!uid,
-        staleTime: 1000 * 60 * 10,
+        // staleTime: 1000 * 60,
       })
 
 
