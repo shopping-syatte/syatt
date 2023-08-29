514e8922a0b07498ac31e055a6c72e22f36696fd
KimGyeongwon <ruddnjs6651@gmail.com>
2023-08-29 17:14:49 +0900

    feat: userHeader 완성

--- src/components/CartStatus.jsx
+++ src/components/CartStatus.jsx
@@ -1,16 +1,44 @@
-import { AiOutlineShoppingCart } from 'react-icons/ai';
 import useCart from '../hooks/useCart.jsx';
+import { MdShoppingCart } from 'react-icons/md';
 
 export default function CartStatus() {
-  const { cartQuery: {data: products}} = useCart();
+  const {
+    cartQuery: { data: products },
+  } = useCart();
 
+  const handleMouseEnter = (event) => {
+    const icon = event.currentTarget.querySelector('.shopping-cart');
+    if (icon) {
+      icon.style.color = '#CFA461';
+    }
+  };
+
+  const handleMouseLeave = (event) => {
+    const icon = event.currentTarget.querySelector('.shopping-cart');
+    if (icon) {
+      icon.style.color = '';
+    }
+  };
 
   return (
-    <div className={'relative'}>
-      <AiOutlineShoppingCart className={'text-4xl'}/>
-      {products &&
-        <p className={'w-6 h-6 text-center bg-brand text-white ' +
-          'rounded-full absolute -top-1 -right-2'}>{products.length}</p>}
+    <div
+      className={
+        'relative bg-gold p-2 rounded-full border-2 border-gold hover:bg-white ease-in-out duration-200 transition-all'
+      }
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <MdShoppingCart className={'shopping-cart text-2xl text-white'} />
+      {products && (
+        <p
+          className={
+            'w-6 h-6 text-center bg-brand text-white ' +
+            'rounded-full absolute -top-1 -right-2 drop-shadow-md'
+          }
+        >
+          {products.length}
+        </p>
+      )}
     </div>
-  )
+  );
 }
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line react/prop-types
-export default function User({ user: {photoURL, displayName}}) {
-
+export default function User({ user: { photoURL, displayName } }) {
   return (
     <div className={'flex items-center shrink-0'}>
-      <img className={'w-10 h-10 rounded-full mr-2'} src={photoURL} alt={displayName} />
+      <img
+        className={'w-10 h-10 rounded-full mr-2 '}
+        src={photoURL}
+        alt={displayName}
+      />
       <span className={'hidden md:block'}>{displayName}</span>
     </div>
-  )
+  );
 }
-
