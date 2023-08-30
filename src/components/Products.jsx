5a4968031868cf6b90f3d3d39f4e948bba62a7a5
pelicanStd <pelican8118@gmail.com>
2023-08-30 14:43:38 +0900

    feat: mainpage 과목별 정렬

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,26 +1,41 @@
 import ProductCard from './ProductCard.jsx';
 import useProducts from '../hooks/useProducts.jsx';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
-Products.propTypes = {
+/*Products.propTypes = {
   category: PropTypes.string,
-};
+};*/
 
-export default function Products({ category }) {
+export default function Products() {
   const {
     productsQuery: { isLoading, error, data: products },
   } = useProducts();
+
+  const category = ['기초포페인팅','메탈이펙트','메탈플러스터']
 
   return (
     <>
       {isLoading && <p>Loading...</p>}
       {error && <p>{error.message}</p>}
-      <ul className={'grid grid-cols-1 md:grid-cols-3 gap-4 p-4'}>
+      {
+        category.map((category)=>(
+          <div key={category}>
+            <h2>{category}</h2>
+            <ul className={'grid grid-cols-1 md:grid-cols-3 gap-4 p-4'}>
+            {products && products.filter((product) => product.section === category)
+              .map((product) => (
+                <ProductCard key={product.id} product={product} />
+              ))}
+            </ul>
+          </div>
+        ))
+      }
+      {/*
         {products &&
           products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
-      </ul>
+      </ul>*/}
     </>
   )
 }
