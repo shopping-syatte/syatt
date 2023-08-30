e716a5a7e8855928b592ad0d145937bd85749ccc
KimGyeongwon <ruddnjs6651@gmail.com>
2023-08-30 21:48:08 +0900

    fix : section 사진 안나오는 문제 해결

--- src/components/HomeSections.jsx
+++ src/components/HomeSections.jsx
@@ -7,13 +7,22 @@ export default function ButtomSections() {
     <>
       <div className="flex justify-center">
         <div className="grid grid-cols-2 w-[1180px] h-[320px] place-items-center gap-3 cursor-pointer">
-          {categorySection.map((section, index) => (
-            <div
-              key={index}
-              className={`w-[590px] h-[160px] bg-section${index + 1}`}
-              onClick={() => navigate(`/products/category/${section}`)}
-            ></div>
-          ))}
+          <div
+            className="w-[590px] h-[160px] bg-section1"
+            onClick={() => navigate(`/products/category/${categorySection[0]}`)}
+          />
+          <div
+            className="w-[590px] h-[160px] bg-section2"
+            onClick={() => navigate(`/products/category/${categorySection[1]}`)}
+          />
+          <div
+            className="w-[590px] h-[160px] bg-section3"
+            onClick={() => navigate(`/products/category/${categorySection[2]}`)}
+          />
+          <div
+            className="w-[590px] h-[160px] bg-section4"
+            onClick={() => navigate(`/products/category/${categorySection[3]}`)}
+          />
         </div>
       </div>
     </>
