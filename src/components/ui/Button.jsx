64a9a23060cf7534ccc799189400db0f4791a491
Geumyeop Yoon <yoongy3333@gmail.com>
2023-09-04 13:02:07 +0900

    style: 공통 button 스타일 추가

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,9 +1,21 @@
-export default function Button({text, onClick}) {
+export default function Button({
+  text,
+  onClick,
+  width = 'auto',
+  height = 'auto',
+}) {
+  const buttonStyle = {
+    width: width,
+    height: height,
+  };
+
   return (
     <button
-      className={'bg-brand py-2 px-4 text-white rounded-sm hover:brightness-110'}
-      onClick={onClick}>
+      className={'bg-gold py-2 px-4 text-white rounded-xl hover:brightness-110'}
+      style={buttonStyle}
+      onClick={onClick}
+    >
       {text}
     </button>
-  )
+  );
 }
